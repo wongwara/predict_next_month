3998cb3084d8df9bde4410188c444f9bdaee0343
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-05-18 19:56:38 +1000

    Create prediction.py

---
+++ prediction.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+## for visualization
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import seaborn as sns
+
+# for datetime column
+from datetime import datetime, date
+
+# avoid the warning display
+import warnings
+warnings.filterwarnings("ignore")

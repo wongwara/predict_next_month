822b3307d25619f5b3236e0f27e776c7e6155dcc
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-05-23 04:29:02 +1000

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -5,19 +5,20 @@ import plotly.express as px
 import plotly.graph_objs as go
 
 def load_data():
-    df = pd.read_csv("https://raw.githubusercontent.com/wongwara/Jobseeker_Baymax/main/dataset/listings2019_2022.csv")
+    df = pd.read_csv("https://raw.githubusercontent.com/wongwara/predict_next_month/main/df_monthly.csv")
     return df
 
 df = load_data()
 
 def show_explore_page():
-    st.title("💰 Job salary for data scientist in AUSTRALIA")
+    st.title("Predict next month")
   
     st.write(
         """ 
-        As a data science student preparing to enter the job market, we were curious about programming languages and whether or not we required a basic understanding of multiple languages, as well as what employers expect of job candidates.
-        """
+       As bank data scientists, we employ machine learning to analyse the huge amount of data available in banking. This data includes transaction history, client information, and meaningful financial information. We can swiftly process and evaluate this massive amount of data by leveraging machine learning algorithms' capabilities, detecting anomalies, and making accurate forecasts.
+       """
     ) 
+    
     st.write(
         """
              Therefore, the objective of this project would be to develop a machine learning model that accepts a job title and the job description with any related words from the job as input and returns expected salary associated with that job.

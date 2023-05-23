4ca4f248a9084ea60d3fafa2955da593c16012ac
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-05-24 06:04:59 +1000

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from explore_page import show_explore_page
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 
 if page == "Predict":
-    show_predict_page()
+    secret_key = "your_secret_key"  # Replace with your own secret key
+
+    entered_key = st.text_input("Enter the secret key", type="password")
+
+    if entered_key == secret_key:
+        st.success("Authentication successful!")
+        show_predict_page()
+        # Add your application logic here
+    else:
+        st.error("Authentication failed!")
 else:
     show_explore_page()

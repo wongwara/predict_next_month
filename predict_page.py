5452262c1904ebd3042b9e3a3621bf3ee551457e
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-05-24 05:48:58 +1000

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -9,10 +9,11 @@ regressor_loaded = data["model"]
 
 
 def show_predict_page():
-    st.title("💵 Predict Next Month")
-    st.write("""Client Financial management: Help clients organise their budget by predicting their spending for the next month. This will help improve financial stability and overall pleasure.""")
-    st.subheader("We need some information to predict your next month")
-
+    st.title("🌸 Application for Client to Predict their next month spending")
+    st.write("""This application is create to Help clients organise their budget by predicting their spending for the next month.""")
+    st.write("""This will help improve financial stability and overall pleasure.""")
+    
+    st.subheader("We need some information to predict your next month spending")
     age = st.number_input('Enter your age', min_value=15, max_value=95, step=1)
     current_month_spending = st.number_input('Enter your current month spending', min_value=0, max_value=20000000, step=1)
     
